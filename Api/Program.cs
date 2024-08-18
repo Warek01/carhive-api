3f80e1d15b9d25d274654396121f7a44be23b5b7
Alexandru Dobrojan <alexandrudobrojan@gmail.com>
2024-08-18 15:32:02 +0300

    refactor: use fluent style in Program

--- Api/Program.cs
+++ Api/Program.cs
@@ -22,11 +22,10 @@ public static class Program {
   private static WebApplicationBuilder _builder = null!;
 
   public static void Main(string[] args) {
-    var culture = CultureInfo.InvariantCulture;
-    CultureInfo.DefaultThreadCurrentCulture = culture;
-    CultureInfo.DefaultThreadCurrentUICulture = culture;
-    Thread.CurrentThread.CurrentCulture = culture;
-    Thread.CurrentThread.CurrentUICulture = culture;
+    CultureInfo.DefaultThreadCurrentCulture =
+      CultureInfo.DefaultThreadCurrentUICulture =
+        Thread.CurrentThread.CurrentCulture =
+          Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
 
     _builder = WebApplication.CreateBuilder(args);
 
@@ -38,25 +37,27 @@ public static class Program {
         c.WriteTo.Console();
       }
     );
-    _builder.Services.AddAutoMapper(typeof(MappingProfile));
     _builder.Services.AddControllers();
-    _builder.Services.AddSingleton<IConfiguration>(_builder.Configuration);
-    _builder.Services.Configure<KestrelServerOptions>(o => {
-      o.Limits.MaxRequestBodySize = 50_000_000;
-      o.Limits.MaxRequestLineSize = 8192;
-    });
-    _builder.Services.AddHttpClient();
-    _builder.Services.AddProblemDetails();
-    _builder.Services.AddExceptionHandler<HttpExceptionHandler>();
-    _builder.Services.AddApiVersioning(options => {
-      options.DefaultApiVersion = new ApiVersion(1);
-      options.ReportApiVersions = true;
-      options.AssumeDefaultVersionWhenUnspecified = true;
-      options.ApiVersionReader = new UrlSegmentApiVersionReader();
-    }).AddApiExplorer(options => {
-      options.GroupNameFormat = "'v'V";
-      options.SubstituteApiVersionInUrl = true;
-    });
+
+    _builder.Services
+      .Configure<KestrelServerOptions>(o => {
+        o.Limits.MaxRequestBodySize = 50_000_000;
+        o.Limits.MaxRequestLineSize = 8192;
+      })
+      .AddAutoMapper(typeof(MappingProfile))
+      .AddSingleton<IConfiguration>(_builder.Configuration)
+      .AddHttpClient()
+      .AddProblemDetails()
+      .AddExceptionHandler<HttpExceptionHandler>()
+      .AddApiVersioning(options => {
+        options.DefaultApiVersion = new ApiVersion(1);
+        options.ReportApiVersions = true;
+        options.AssumeDefaultVersionWhenUnspecified = true;
+        options.ApiVersionReader = new UrlSegmentApiVersionReader();
+      }).AddApiExplorer(options => {
+        options.GroupNameFormat = "'v'V";
+        options.SubstituteApiVersionInUrl = true;
+      });
 
     var fileProvider = new PhysicalFileProvider(_builder.Environment.WebRootPath);
 
@@ -73,41 +74,45 @@ public static class Program {
 
     var app = _builder.Build();
 
-    app.UseSerilogRequestLogging();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseHttpsRedirection();
-    app.UseExceptionHandler();
-    app.UseFileServer();
-    app.UseStaticFiles(new StaticFileOptions {
-      RequestPath = StaticFileService.RequestPath,
-      HttpsCompression = HttpsCompressionMode.Compress,
-      ServeUnknownFileTypes = false,
-      RedirectToAppendTrailingSlash = false,
-      FileProvider = fileProvider,
-    });
+    app
+      .UseSerilogRequestLogging()
+      .UseSwagger()
+      .UseSwaggerUI()
+      .UseHttpsRedirection()
+      .UseExceptionHandler()
+      .UseFileServer()
+      .UseStaticFiles(new StaticFileOptions {
+        RequestPath = StaticFileService.RequestPath,
+        HttpsCompression = HttpsCompressionMode.Compress,
+        ServeUnknownFileTypes = false,
+        RedirectToAppendTrailingSlash = false,
+        FileProvider = fileProvider,
+      });
+
     AuthService.SetupAuthorization(app);
     app.MapControllerRoute("Default", "{controller}/{action}/{id?}");
 
     if (app.Environment.IsDevelopment()) {
-      app.UseCors(options => {
-        options.AllowAnyOrigin();
-        options.AllowAnyHeader();
-        options.AllowAnyMethod();
-      });
-      app.UseDirectoryBrowser(new DirectoryBrowserOptions {
-        FileProvider = fileProvider,
-        RequestPath = "/DirectoryBrowser",
-        RedirectToAppendTrailingSlash = false,
-      });
-      app.UseDefaultFiles();
-      app.UseStaticFiles(new StaticFileOptions {
-        RequestPath = "/DirectoryBrowser",
-        HttpsCompression = HttpsCompressionMode.Compress,
-        ServeUnknownFileTypes = true,
-        RedirectToAppendTrailingSlash = false,
-        FileProvider = fileProvider,
-      });
+      app
+        .UseCors(options => {
+          options.AllowAnyOrigin();
+          options.AllowAnyHeader();
+          options.AllowAnyMethod();
+        })
+        .UseDirectoryBrowser(new DirectoryBrowserOptions {
+          FileProvider = fileProvider,
+          RequestPath = "/DirectoryBrowser",
+          RedirectToAppendTrailingSlash = false,
+        })
+        .UseDefaultFiles()
+        .UseStaticFiles(new StaticFileOptions {
+          RequestPath = "/DirectoryBrowser",
+          HttpsCompression = HttpsCompressionMode.Compress,
+          ServeUnknownFileTypes = true,
+          RedirectToAppendTrailingSlash = false,
+          FileProvider = fileProvider,
+        });
+
       app.MapGet("/", ctx => {
         ctx.Response.Redirect("swagger/index.html");
         return Task.CompletedTask;
@@ -120,76 +125,83 @@ public static class Program {
   private static void SetupAuthentication() {
     var jwtConfig = new JwtConfig();
     _builder.Configuration.Bind("Jwt", jwtConfig);
-    _builder.Services.AddSingleton(jwtConfig);
 
-    _builder.Services.AddAuthentication(options => {
-      options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-      options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-      options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-      options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
-      options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
-    }).AddJwtBearer(options => {
-      options.TokenValidationParameters = AuthService.CreateTokenValidationParameters(jwtConfig);
-      options.MapInboundClaims = false;
-    });
+    _builder.Services
+      .AddSingleton(jwtConfig)
+      .AddAuthentication(options => {
+        options.DefaultScheme =
+          options.DefaultChallengeScheme =
+            options.DefaultAuthenticateScheme =
+              options.DefaultSignInScheme =
+                options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+      })
+      .AddJwtBearer(options => {
+        options.TokenValidationParameters = AuthService.CreateTokenValidationParameters(jwtConfig);
+        options.MapInboundClaims = false;
+      });
+
     _builder.Services.AddAuthorization();
   }
 
   private static void SetupSwagger() {
-    _builder.Services.AddEndpointsApiExplorer();
-    _builder.Services.AddSwaggerGen(options => {
-      options.SwaggerDoc("v1", new OpenApiInfo {
-        Version = "v1",
-        Title = "CarHive API"
+    _builder.Services
+      .AddEndpointsApiExplorer()
+      .AddSwaggerGen(options => {
+        options.SwaggerDoc("v1", new OpenApiInfo {
+          Version = "v1",
+          Title = "CarHive API"
+        });
+
+        var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
+        var securityDefinition = new OpenApiSecurityScheme {
+          Name = "Bearer",
+          BearerFormat = "JWT",
+          Scheme = "bearer",
+          Description = "Specify the authorization token.",
+          In = ParameterLocation.Header,
+          Type = SecuritySchemeType.Http
+        };
+        options.AddSecurityDefinition("jwt_auth", securityDefinition);
+
+        var securityScheme = new OpenApiSecurityScheme {
+          Reference = new OpenApiReference {
+            Id = "jwt_auth",
+            Type = ReferenceType.SecurityScheme
+          }
+        };
+        var securityRequirements = new OpenApiSecurityRequirement {
+          [securityScheme] = []
+        };
+        options.AddSecurityRequirement(securityRequirements);
+        options.OperationFilter<CustomSwaggerOperationFilter>();
       });
-
-      var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-      options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-
-      var securityDefinition = new OpenApiSecurityScheme {
-        Name = "Bearer",
-        BearerFormat = "JWT",
-        Scheme = "bearer",
-        Description = "Specify the authorization token.",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http
-      };
-      options.AddSecurityDefinition("jwt_auth", securityDefinition);
-
-      var securityScheme = new OpenApiSecurityScheme {
-        Reference = new OpenApiReference {
-          Id = "jwt_auth",
-          Type = ReferenceType.SecurityScheme
-        }
-      };
-      var securityRequirements = new OpenApiSecurityRequirement {
-        [securityScheme] = []
-      };
-      options.AddSecurityRequirement(securityRequirements);
-      options.OperationFilter<CustomSwaggerOperationFilter>();
-    });
   }
 
   private static void SetupDataSource() {
     var dataSourceBuilder = new NpgsqlDataSourceBuilder(_builder.Configuration.GetConnectionString("Default"));
 
-    dataSourceBuilder.MapEnum<CarFuelType>();
-    dataSourceBuilder.MapEnum<CarBodyStyle>();
-    dataSourceBuilder.MapEnum<CarColor>();
-    dataSourceBuilder.MapEnum<UserRole>();
-    dataSourceBuilder.MapEnum<CarStatus>();
-    dataSourceBuilder.MapEnum<CarDrivetrain>();
-    dataSourceBuilder.MapEnum<CarTransmission>();
-    dataSourceBuilder.MapEnum<ListingStatus>();
-    dataSourceBuilder.MapEnum<ListingAction>();
-    dataSourceBuilder.MapEnum<OauthIdentityProvider>();
-    dataSourceBuilder.MapEnum<ReportType>();
+    dataSourceBuilder
+      .MapEnum<CarFuelType>()
+      .MapEnum<CarBodyStyle>()
+      .MapEnum<CarColor>()
+      .MapEnum<UserRole>()
+      .MapEnum<CarStatus>()
+      .MapEnum<CarDrivetrain>()
+      .MapEnum<CarTransmission>()
+      .MapEnum<ListingStatus>()
+      .MapEnum<ListingAction>()
+      .MapEnum<OauthIdentityProvider>()
+      .MapEnum<ReportType>();
+
     dataSourceBuilder.EnableDynamicJson();
     dataSourceBuilder.EnableParameterLogging();
 
     NpgsqlDataSource dataSource = dataSourceBuilder.Build();
-    _builder.Services.AddDbContext<CarHiveDbContext>(options => { options.UseNpgsql(dataSource); });
-    _builder.Services.AddScoped<DbContext, CarHiveDbContext>();
+    _builder.Services
+      .AddDbContext<CarHiveDbContext>(options => { options.UseNpgsql(dataSource); })
+      .AddScoped<DbContext, CarHiveDbContext>();
   }
 
   private static void SetupCache() {
